fc5e47a86cf50fae9cee2259481c6d00be1c1278
sam <samuelchan@qq.com>
2021-08-01 12:12:39 +0800

    Upgraded the versions: phase 1

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/AdditionalWebConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/AdditionalWebConfig.java
@@ -23,7 +23,7 @@ public class AdditionalWebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
