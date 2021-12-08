7c9e3e6f7345382f38e2efa2e84afe6d288b9caf
sam <samuelchan@qq.com>
2021-12-08 12:00:21 +0800

    Introduced Lombok to reduce Getter and Setter lines. Reuse the JsonUtil

--- src/main/java/cn/gzten/jwt/controller/ResourceController.java
+++ src/main/java/cn/gzten/jwt/controller/ResourceController.java
@@ -5,8 +5,12 @@ import cn.gzten.jwt.repository.UserRepository;
 import cn.gzten.jwt.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+import java.util.function.Supplier;
 
 import static cn.gzten.jwt.domain.Role.ROLE_ADMIN;
 
@@ -33,5 +37,29 @@ public class ResourceController {
         }
         return Mono.just(ou.get());
 
+    }
+
+    /**
+     * For reference, maybe in the future need it.
+     * @param ou
+     * @param function
+     * @param <R>
+     * @return
+     */
+    public static final  <R> Mono<R> checkAuthorityAndReturn(final Optional<User> ou, final Supplier<Mono<R>> function) {
+        return ReactiveSecurityContextHolder.getContext().flatMap(sc -> {
+            if (sc.getAuthentication().getAuthorities().contains(ROLE_ADMIN)) {
+                return function.get();
+            } else {
+                if (!ou.isEmpty() && ou.get().getUsername().equals(sc.getAuthentication().getPrincipal())) {
+                    return function.get();
+                }
+
+                /**
+                 * If it is empty, you will want to prompt 403 because you don't want ppl to know this user id not exist.
+                 */
+                throw new AppException(403, "Not authorized to resource!");
+            }
+        });
     }
 }
